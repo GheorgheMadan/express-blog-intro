694762081ccd074f48440d5a19d039b5579e985c
Gheorghe <madangheorghe.30@gmail.com>
2025-02-04 14:35:11 +0100

    first route /

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const port = 3000;
 
 // definisco l'uso di una cartella per i file statici 
 app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.send('Server del mio blog');
+})
+
+app.listen(port, () => {
+    console.log(`l'app è in ascolto sul port ${port}`)
+});
